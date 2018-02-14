7696d6ccfa01efce57d689611cd8f2ad4842fb2d
Barry Bradley <bbradley13@gmail.com>
2018-02-14 00:42:40 +0000

    Added show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   #Shows the index.html.erb view from the static_pages controller 
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
