8624c1a60c313d22eef31bab52f5e3d87910aebc
Barry Bradley <bbradley13@gmail.com>
2018-02-24 23:08:34 +0000

    Adding ability to enroll in course by student

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   #Shows the index.html.erb view from the static_pages controller 
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lessons, only: [:show]
   namespace :instructor do
