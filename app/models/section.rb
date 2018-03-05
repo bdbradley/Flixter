ab8f6a17a7ad3fdb359ebd07b9ab57a4eaa51c15
Barry Bradley <bbradley13@gmail.com>
2018-03-05 04:40:38 +0000

    Editing footer style

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,6 +5,7 @@ class Section < ApplicationRecord
   include RankedModel
   ranks :row_order, with_same: :course_id
 
+  #Says if there isn't a next_section then use the section from the next lesson instead
   def next_lesson
     lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
     if lesson.blank? && section.next_section
