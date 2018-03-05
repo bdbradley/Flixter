b0b0e0eff2c0a0d75a117ce0a8e63f2b463efbc2
Barry Bradley <bbradley13@gmail.com>
2018-03-05 04:21:54 +0000

    Adding next lesson functionality to sections

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,11 @@ class Lesson < ApplicationRecord
   #Should rank lessons in database
    include RankedModel
    ranks :row_order, with_same: :section_id
+
+   def next_lesson
+    #Returns the next lesson in the section,, if not returns nill
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    return lesson
+   end
+
 end
